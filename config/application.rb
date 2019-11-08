0bdded8d4c9e02bf63c38360671a8dd241a502de
Luis Nieto <luisarturonietohdz@gmail.com>
2019-11-08 06:06:07 -0600

    Configurar zona horaria

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module OClock
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.time_zone = 'Mexico City'
+    config.active_record.default_timezone = :local # Or :utc
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
